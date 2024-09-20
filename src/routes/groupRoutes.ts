4d003146de229635a8360f43f6411a5dacc7e270
mir ziauddin <mirziauddin57@gmail.com>
2024-09-20 19:38:22 +0530

    error change

--- src/routes/groupRoutes.ts
+++ src/routes/groupRoutes.ts
@@ -14,13 +14,11 @@ import { adminAuthMiddleware } from "../middleware/adminAuthMiddleware";
 const router = Router();
 
 // Routes
-<<<<<<< HEAD
 router.post("/", adminAuthMiddleware, createGroupController);
 router.get("/getAll", getAllGroupsController);
-=======
+
 router.post("/:adminId", adminAuthMiddleware, createGroupController);
 router.get("/", getAllGroupsController);
->>>>>>> 197b59822f02f8e5d670aabfdcf432b08a644c7a
 router.get("/:id", getGroupByIdController);
 router.put("/:id", adminAuthMiddleware, updateGroupController);
 router.delete("/:id", adminAuthMiddleware, deleteGroupByIdController);
